e92f6a0a2da2adfc18365d9fff57a82952a08d43
JasonHuang <lohas821019@gmail.com>
2022-04-11 11:40:52 +0800

    set the motor can start and stop

--- L298N_BTS7960/L298N_BTS7960.ino
+++ L298N_BTS7960/L298N_BTS7960.ino
@@ -3,6 +3,8 @@
 int PWM = 4;  //pulse/
 int INA1 = 5; // 方向
 int INA2 = 6; // 方向
+char cmd[10];
+int recv;
 
 // L/H 正轉
 // H/L 反轉
@@ -11,26 +13,43 @@ int INA2 = 6; // 方向
 void setup() {
   Serial.begin(9600);
 
+  //初始設定
   pinMode(INA1, OUTPUT);
   pinMode(INA2, OUTPUT);
+//  digitalWrite(PWM, HIGH);
 
+  //設定正反轉
   digitalWrite(INA1, LOW);
-  digitalWrite(INA2, HIGH);
-  digitalWrite(PWM, LOW);
-
+  digitalWrite(INA2, LOW);
 }
 
 void loop() {
-  
-  steptimes();
-  
-}
 
-void  steptimes( ) {
-  for (int counter = 0; counter <= 200; counter++) {
-    //digitalWrite(PWM, HIGH);
-    delayMicroseconds(1000);
-    //digitalWrite(PWM, LOW);
-    delayMicroseconds(1000);
+  if (recv == 0) {
+    digitalWrite(INA1, LOW);
+    digitalWrite(INA2, LOW);
+  }
+  else if (recv == 1) {
+    digitalWrite(INA1, LOW);
+    digitalWrite(INA2, HIGH);
+  }
+  else {
+    digitalWrite(INA1, HIGH);
+    digitalWrite(INA2, LOW);
+  }
+
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    Serial.readBytes(cmd, 10);
+    recv = atoi(cmd);
+    //這邊沒有值的時候會一直收到0，所以上方recv == 0，要都設定成LOW
+    Serial.println(atoi(recv));
+//    steptimes();
   }
 }
+
+//void  steptimes( ) {
+//  for (int counter = 0; counter <= 200; counter++) {
+//    delayMicroseconds(2000);
+//  }
+//}
